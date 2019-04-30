8f92a77a3f14aade8a4a0728a3b054d34815aeb4
Robert Macaibay <rtmacaibay@usfca.edu>
2019-04-30 10:36:20 -0700

    Finished proj + docs

--- allocator.c
+++ allocator.c
@@ -25,7 +25,7 @@
 #include <pthread.h>
 
 #ifndef DEBUG
-#define DEBUG 1
+#define DEBUG 0
 #endif
 
 #define LOG(fmt, ...) \
@@ -62,10 +62,12 @@ struct mem_block {
 
 /* Start (head) of our linked list: */
 struct mem_block *g_head = NULL;
+/* End (tail) of our linked list: */
 struct mem_block *g_tail = NULL;
 
 /* Allocation counter: */
 unsigned long g_allocations = 0;
+
 
 pthread_mutex_t g_alloc_mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -100,6 +102,13 @@ void print_memory(void) {
     }
 }
 
+/**
+ * write_memory
+ *
+ * Writes out the current memory state to a file, including both the regions and blocks.
+ * Entries are printed in order, so there is an implied link from the topmost
+ * entry to the next, and so on.
+ */
 void write_memory(FILE * fp) {
     fputs("-- Current Memory State --", fp);
     struct mem_block *current_block = g_head;
@@ -137,19 +146,30 @@ void write_memory(FILE * fp) {
  * Returns: new memory block pointer
  */
 void *reuse_space(struct mem_block * ptr, size_t real_sz) {
+    //check if our current block can be reused without splitting
     if (ptr->usage == 0) {
+        //change usage to real_sz
         ptr->usage = real_sz;
+        //copy the old id
         unsigned long old_id = ptr->alloc_id;
+        //change alloc_id to new allocation
         ptr->alloc_id = g_allocations++;
+        //log msg telling us we're using a certain alloc
         LOG("Allocation request USING reuse; reusing alloc %ld\n", old_id);
+        //return the "new" pointer
         return ptr + 1;
     }
+
+    //create a new mem_block that encompasses the rest of the space
     struct mem_block * freeloader = (void*)ptr + ptr->usage;
+    //explanatory struct stuff
     freeloader->alloc_id = g_allocations++;
     freeloader->size = ptr->size - ptr->usage;
     freeloader->usage = real_sz;
     freeloader->region_start = ptr->region_start;
     freeloader->region_size = ptr->region_size;
+
+    //update the linked list
     if (g_tail == ptr) {
         freeloader->next = NULL;
         g_tail->next = freeloader;
@@ -159,8 +179,14 @@ void *reuse_space(struct mem_block * ptr, size_t real_sz) {
         ptr->next = freeloader;
     }
 
+    //update previous pointer's size
     ptr->size = ptr->usage;
 
+    //log msg to tell us we're reusing space somewhere
+    LOG("Allocation request USING reuse;\n size = %zu, alloc = %ld, where?: %ld\n", 
+        real_sz, freeloader->alloc_id, ptr->alloc_id);
+
+    //return new pointer
     return freeloader + 1;
 }
 
@@ -175,44 +201,51 @@ void *reuse_space(struct mem_block * ptr, size_t real_sz) {
  * Returns: memory block pointer with size
  */
 void *reuse(size_t size) {
+    //if g_head is NULL, we got nothing to reuse
     if (g_head == NULL) {
         return NULL;
     }
 
+    //check what kind of algorithm we're using
     char * algo = getenv("ALLOCATOR_ALGORITHM");
     if (algo == NULL) {
         algo = "first_fit";
     }
 
-    //LOG("Algo: %s\n", algo);
-
+    //keeps track of size that fits our criteria
     size_t fits_best = SIZE_MAX;
-    size_t fits_worst = 1;
+    size_t fits_worst = 0;
 
+    //keeps track of the pointer we want to reuse
     struct mem_block * the_spot = NULL;
 
+    //curr to iterate thru linked list
     struct mem_block * curr = g_head;
+
+    //gather real_sz
     size_t real_sz = size + sizeof(struct mem_block);
 
-    LOG("Allocation request USING reuse; size = %zu, alloc = %ld\n", real_sz, g_allocations);
-
+    //iterate thru linked list until we're done or find something
     while (curr != NULL) {
+        //this is just the free space of a current block
         size_t free_space = curr->size - curr->usage;
 
+        //check for what kind of algorithm we want
         if (strcmp(algo, "first_fit") == 0) {
+            //if there's space, take it
             if (curr->usage < curr->size && real_sz <= free_space) {
                 return reuse_space(curr, real_sz);
             }
         } else if (strcmp(algo, "best_fit") == 0) {
+            //if there's space, check if it fits best
             if (curr->usage < curr->size && real_sz <= free_space) {
-                LOG("Possible space: %zu\n", free_space);
                 if (free_space < fits_best) {
-                    //LOG("New space: %zu\n", free_space);
                     fits_best = free_space;
                     the_spot = curr;
                 }
             }
         } else if (strcmp(algo, "worst_fit") == 0) {
+            //if there's space, check if it has a lot of free space
             if (curr->usage < curr->size && real_sz <= free_space) {
                 if (free_space > fits_worst) {
                     fits_worst = free_space;
@@ -220,18 +253,29 @@ void *reuse(size_t size) {
                 }
             }
         }
+
+        //thank u, next
         curr = curr->next;
     }
 
+    //if this isn't NULL, that means we got something
     if (the_spot != NULL) {
-        size_t free_space = the_spot->size - the_spot->usage;
-        LOG("Using this spot: %zu\n", free_space);
         return reuse_space(the_spot, real_sz);
     }
 
+    //we return NULL when we can't reuse
     return NULL;
 }
 
+/**
+ * scribble_this
+ * 
+ * Literally scribbles the memory in a pointer
+ *
+ * Parameters:
+ * - ptr: pointer we want to scribble
+ * - size: bytes to scribble
+ */
 void scribble_this(void * ptr, size_t sz) {
     memset(ptr, 0xAA, sz);
 }
@@ -247,11 +291,12 @@ void scribble_this(void * ptr, size_t sz) {
  * Returns memory block pointer with allocated memory.
  */
 void *malloc(size_t size) {
+    //lock immediately lul
     pthread_mutex_lock(&g_alloc_mutex);
-    //LOG("Allocation request; size = %zu\n", size);
+
+    //check if we should scribble
     char * s = getenv("ALLOCATOR_SCRIBBLE");
     int scribble = 0;
-
     if (s != NULL) {
         scribble = atoi(s);
     }
@@ -261,19 +306,20 @@ void *malloc(size_t size) {
         size = (((int) size / 8) * 8) + 8;
     }
 
+    //check if we can reuse something
     void * ptr = reuse(size);
-    if (ptr == NULL) {
-        //LOGP("Reuse returned NULL\n");
-    }
     if (ptr != NULL) {
-        // noice we could reuse space 
+        //noice we could reuse space 
+        //scribble if we want to
         if (scribble) {
             scribble_this(ptr, size);
         }
+
+        //unlock mutex
         pthread_mutex_unlock(&g_alloc_mutex);
         return ptr;
     }
-
+    //log msg to let us know there's a new request
     LOG("Allocation request; size = %zu\n", size);
 
     /* How much space we are using in the region */
@@ -293,7 +339,7 @@ void *malloc(size_t size) {
 
     /* Size of entire region */
     size_t region_sz = num_pages * page_sz;
-    //LOGP("Mapping new region\n");
+
     /* Set up memory block */
     struct mem_block * block = mmap(
             NULL, /* Address (we use NULL to let the kernel decide) */
@@ -303,12 +349,14 @@ void *malloc(size_t size) {
             -1, /* file descriptor */
             0 /* offset to start at within the file */);
 
+    //if MAP_FAILED, rip
     if (block == MAP_FAILED) {
         pthread_mutex_unlock(&g_alloc_mutex);
         perror("mmap");
         return NULL;
     }
 
+    //struct stuff, don't really care
     block->alloc_id = g_allocations++;
     block->size = region_sz;
     block->usage = real_sz;
@@ -326,11 +374,15 @@ void *malloc(size_t size) {
         g_tail->next = NULL;
     }
 
+    //scribble?
     if (scribble) {
         scribble_this(block + 1, size);
     }
 
+    //unlock
     pthread_mutex_unlock(&g_alloc_mutex);
+
+    //haha block + 1 bc we don't want user to write over struct
     return block + 1;
 }
 
@@ -345,11 +397,14 @@ void *malloc(size_t size) {
  * Returns void
  */
 void destroy_this(struct mem_block * ptr) {
+    //munmap that pointer
     int ret = munmap(ptr->region_start, ptr->region_size);
-    //LOGP("Free request; Destroyed a pointer\n");
     if (ret == -1) {
         perror("munmap");
     }
+
+    //log msg bc i want to know if we destroyed something
+    LOGP("Free request; Destroyed a pointer\n");
 }
 
 /**
@@ -364,28 +419,37 @@ void destroy_this(struct mem_block * ptr) {
  * Returns: void
  */
 void free(void *ptr) {
+    //lock immediately lul
     pthread_mutex_lock(&g_alloc_mutex);
 
+    //we can't free something that isn't there LMAO
     if (ptr == NULL) {
-        /* Freeing a NULL pointer does nothing */
         pthread_mutex_unlock(&g_alloc_mutex);
         return;
     }
 
+    //get the struct
     struct mem_block * blk = (struct mem_block *) ptr - 1;
+    //log msg telling us we are removing something
     LOG("Free request; size = %zu, alloc = %lu\n", blk->usage, blk->alloc_id);
     
+    //update usage to 0
     blk->usage = 0;
 
+    //keep track of region start
     struct mem_block * orig_start = blk->region_start;
+    //prev to iterate thru linked list
     struct mem_block * prev = g_head;
+    //keep track of linked list parts to connect them
     struct mem_block * first_half = NULL;
     struct mem_block * sec_half = NULL;
+    //first node or nah
     int first_node = 0;
 
     //if first node is our block
     if (prev->region_start == orig_start) {
         first_node = 1;
+        //wouldn't it be crazy if the first node had something
         if (prev->usage != 0) {
             pthread_mutex_unlock(&g_alloc_mutex);    
             return;
@@ -406,33 +470,41 @@ void free(void *ptr) {
             } 
 
             if (curr->usage != 0) {
-                //LOGP("Free request; Completed - block still in use\n");
+                //log msg with explanatory msg
+                LOGP("Free request; Completed - block still in use\n");
                 pthread_mutex_unlock(&g_alloc_mutex);
                 return;
             }
         } else if (first_half != NULL) {
+            //if we get a new region, save that as our other half
             sec_half = curr;
             break;
         }
+
+        //thank u, next
         prev = prev->next;
     }
 
-    /* I WILL FINSIH THIS ELIJAH */
+    //this is to update our linked list yuuuhh
     if (first_node) {
         if (sec_half == NULL) {
+            /* if we in here, remove entire linked list */
             destroy_this(blk);
             g_head = NULL;
             g_tail = NULL;
         } else {
+            /* if we in here, remove top half of linked list */
             destroy_this(blk);
             g_head = sec_half;
         }
     } else if (first_half != NULL) {
         if (sec_half == NULL) {
+            /* if we in here, remove bottom half of linked list */
             destroy_this(blk);
             g_tail = first_half;
             g_tail->next = NULL;
         } else {
+            /* if we in here, connect halves of linked list */
             destroy_this(blk);
             first_half->next = sec_half;
         }
@@ -440,8 +512,10 @@ void free(void *ptr) {
         perror("free");
     }
 
+    //unlock
     pthread_mutex_unlock(&g_alloc_mutex);
 
+    //pls let me know if we survived free
     LOGP("Free request; Completed\n");
 }
 
@@ -457,15 +531,18 @@ void free(void *ptr) {
  *
  * Returns memory block pointer with allocated memory.
  */
-void *calloc(size_t nmemb, size_t size)
-{
+void *calloc(size_t nmemb, size_t size) {
+    //5 times 0 is still 0
     if (nmemb == 0 || size == 0) {
         return NULL;
     }
 
+    //malloc that
     void * ptr = malloc(nmemb * size);
+    //init that to 0
     memset(ptr, 0, nmemb * size);
 
+    //ret
     return ptr;
 }
 
@@ -500,18 +577,25 @@ void *realloc(void *ptr, size_t size) {
         return NULL;
     }
 
-    // TODO: reallocation logic
+    //get mem_block struct
     struct mem_block * blk = (struct mem_block *) ptr - 1;
 
+    //check if there's space to just use
     if (blk->size >= size) {
+        //change usage to new size
         blk->usage = size;
         return ptr;
     } else {
+        //malloc a new pointer
         void * new = malloc(size);
+        //copy those contents
         memcpy(new, ptr, blk->usage);
+        //free old pointer
         free(ptr);
+        //return new pointer
         return new;
     }
 
+    //return NULL if we end up dying
     return NULL;
 }
